12692e7b43a3f3b6c80636a08aeb51a4475e7f04
federico2277 <101802997+federico2277@users.noreply.github.com>
2022-12-14 10:46:43 +0100

    Add files via upload

--- Progetto/provaserver/src/main/java/com/example/ClientHandler.java
+++ Progetto/provaserver/src/main/java/com/example/ClientHandler.java
@@ -1,10 +1,12 @@
 package com.example;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientHandler extends Thread {
     private Socket s;
@@ -32,7 +34,7 @@ public class ClientHandler extends Thread {
 
     //crezioni variabili    
         boolean ciclo = true;
-        Boolean ciclop= true;
+        Boolean ciclop = true;
 
         try {   
         //inserimento del hostname
@@ -43,43 +45,50 @@ public class ClientHandler extends Thread {
         //ricezzione tabella se vuole essere publico o privato la connesione
                 
                 while (ciclop) {
-                out.println(" Vuoi comunicare in pubblico o privato ?");
-                sessione = in.readLine();
-                System.out.println(sessione);
-                //sessione privata
-                if (sessione.equalsIgnoreCase("privato")){
-                    out.println("scrivi il nome della persona con cui vuoi chattare se vuoi chiudere la chat digita (esci da chat)");
-                    String destinatario=in.readLine(); 
-                    out.println("scrivi il messagio");
+
+                    out.println(" Vuoi comunicare in pubblico o privato ?");
+                    sessione = in.readLine();
+                    System.out.println(sessione);
+                    //sessione privata
+                        if (sessione.equalsIgnoreCase("privato")){
+                            ciclo = true;
+                            out.println("scrivi il nome della persona con cui vuoi chattare se vuoi chiudere la chat digita (esci da chat)");
+                            String destinatario=in.readLine(); 
+                            out.println("scrivi il messagio");
+                            while(ciclo){
+                                String messaggio=in.readLine(); 
+                                if(messaggio.equals("esci da chat")){
+                                    sessione = "vuota";
+                                    ciclo=false;}
+                                msg=new MessagioMaranzone(hostname, destinatario, messaggio,sessione);
+                                invioSingolo(msg);
+                                ObjectMapper objectMapper = new ObjectMapper();
+                                objectMapper.writeValue(new File("provaserver/target/Maranzone.json"), msg);
+                            }
+
+
+                        }//sessione pubblica
+                    else if (sessione.equalsIgnoreCase("pubblico")) {
+                        ciclo = true;
+                        out.println("se vuoi uscire scrivi:esci da chat");
+                        out.println("inserire cosa vuore dire hai fra");
+
                     while(ciclo){
-                    String messaggio=in.readLine(); 
-                    if(messaggio.equals("esci da chat")){ciclo=false;break;}
-                    msg=new MessagioMaranzone(hostname, destinatario, messaggio,sessione);
-                    invioSingolo(msg);
-                   }
-
-
-                }//sessione publica
-                else if (sessione.equalsIgnoreCase("publico")) {
-                    out.println("se vuoi uscire scrivi:esci da chat");
-                    out.println("inserire cosa vuore dire hai fra");
-
-                   while(ciclo){
-                        String messagio=in.readLine(); 
-                        if(messagio.equalsIgnoreCase("esci da chat")){ciclo=false;}
-                        msg=new MessagioMaranzone(hostname, messagio, sessione);
-                        sendToAll(msg);
+                            String messagio=in.readLine(); 
+                            if(messagio.equalsIgnoreCase("esci da chat")){ciclo=false;}
+                            msg=new MessagioMaranzone(hostname, messagio, sessione);
+                            sendToAll(msg);
+                        
+                        }    
                     
-                    }    
-                   
-                }//uscita dalla sessione
-                else if (sessione.equalsIgnoreCase("uscire")) {
-                    
-                    out.println("Addio ti ho voluto pene mon amour");
-                    sendToAll("@");
-                    ciclo = false;
-                }//messagio non valido
-                else{out.println("Non è valido questo comando");}
+                    }//uscita dalla sessione
+                    else if (sessione.equalsIgnoreCase("uscire")) {
+                        
+                        out.println("Addio ti ho voluto pene mon amour");
+                        sendToAll("@");
+                        ciclo = false;
+                    }//messagio non valido
+                    else{out.println("Non è valido questo comando");}
             }
         } catch (Exception e) {
        
@@ -105,14 +114,33 @@ public class ClientHandler extends Thread {
             }
         }
     }
+
     private void invioSingolo(MessagioMaranzone maranzone) {
+        if(maranzone.getMsg().equals("esci da chat")){ 
+            
+         }else{
         for(int i=0;i<clients.size();i++){
             if(clients.get(i).getHostname().equalsIgnoreCase(maranzone.getDestinatario())){
                 clients.get(i).out.println(maranzone.getMitente()+":"+maranzone.getMsg());
             }
         }
+        }
     }
 
+    private String controlloSessione(MessagioMaranzone maranzone){
+
+        for(int i=0;i<clients.size();i++){
+            if (clients.get(i).getSessione().equals("vuota") ) {
+                return maranzone.getDestinatario() +" ha lasciato la sessione ";
+            }else if(clients.get(i).getSessione().equals("pubblica")){
+                return maranzone.getDestinatario() +" ha lasciato la sessione ";
+
+            }
+            }
+
+
+        return null;
+    }
 
     public Socket getS() {
         return s;
